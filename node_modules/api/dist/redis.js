a6b41e452ddefab87a0728d9666fabb224cb54f4
djdreamfix <226608340+djdreamfix@users.noreply.github.com>
2026-01-21 22:28:17 +0200

    Delete node_modules directory

--- node_modules/api/dist/redis.js
+++
@@ -1,6 +0,0 @@
-import { Redis } from "@upstash/redis";
-export const redis = Redis.fromEnv();
-// Keys:
-// mark:{id} -> HASH-like via JSON (stored as string)
-// marks_by_expiry -> ZSET score = expiresAt (ms)
-// push:subs -> SET of JSON strings or a LIST
